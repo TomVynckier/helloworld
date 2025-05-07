d2d7c5addc540c6dd5d95c335698c879530a72b5
TomVynckier <tom.vynckier@galloo.com>
2025-05-07 20:53:20 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "<img src="https://www.rodsnsods.co.uk/media/2013-big-boob-bounce-gif.339734/full" alt="Girl in a jacket" width="500" height="600">");
+app.MapGet("/", () => "Hallo");
 
 
 app.Run();
